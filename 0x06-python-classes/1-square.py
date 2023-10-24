74edbd4b91b9c6146c836125ab58199723815940
Clevoh <clevelihasi@gmail.com>
2023-10-24 08:48:58 -0700

    python_classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" this module defines a class square """
+
+
+class Square:
+    """class for Square"""
+    def __init__(self, size):
+        """initialize class"""
+        self.__size = size
+
+
