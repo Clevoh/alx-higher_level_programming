1e4769cd5a07fc94d4a303e577d652203e824974
Clevoh <clevelihasi@gmail.com>
2023-11-07 11:30:38 -0800

    update

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 """returns dictionary description with sample data structure"""
 
-def class_to_json(obj):
-    """ retuns the dictionary description with simple data structure """
 
-    structure = {}
-    if hasattr(obj, "__dict__"):
-        structure = obj.__dict__.copy()
-    return structure
+def class_to_json(obj):
+    """returns dictionary decription with sample data structure"""
+    return obj.__dict__
