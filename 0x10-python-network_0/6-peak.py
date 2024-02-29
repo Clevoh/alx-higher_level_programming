ec2fbd5d5f2a5bb57e68b4dd80ae15b9b17a0029
Clevoh <clevelihasi.com>
2024-02-29 11:03:55 -0800

    6. Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 
 def find_peak(list_of_integers):
-    if list_of_integers == []:
+    if len(list_of_integers) == 0:
         return None
-
-    size = len(list_of_integers)
-    if size == 1:
+    if len(list_of_integers) == 1:
         return list_of_integers[0]
-    elif size == 2:
+    if len(list_of_integers) == 2:
         return max(list_of_integers)
 
-    mid = int(size / 2)
+    mid = int(len(list_of_integers) / 2)
     peak = list_of_integers[mid]
     if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
         return peak
