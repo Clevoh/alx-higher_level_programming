383f73017bf575de6e1dc8d7ae8b15bbc0a1703a
Clevoh <clevelihasi.com>
2024-03-01 01:55:49 -0800

    3. Error code #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,14 +1,14 @@
-#!/user/bin/bash
-"""ssend post request with mail"""
+#!/usr/bin/python3
+"""send a post requestwith mail"""
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import urllib.request
     import urllib.parse
     import sys
 
-    lis = aya.argv
-    values = { }
-    values = ['email'] = lis[2]
+    lis = sys.argv
+    values = {}
+    values['email'] = lis[2]
     data = urllib.parse.urlencode(values)
     data = data.encode('ascii')
     req = urllib.request.Request(lis[1], data)
